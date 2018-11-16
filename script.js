af54ebee323662fa593507416744970deda090be
Nancy Lombardi <Nancy@Nancys-MacBook-Pro.local>
2018-11-16 11:06:05 -0500

    latest updates

---
+++ script.js
@@ -0,0 +1,28 @@
+var array = 'vgh';
+
+// function answerZero() {
+// 	$(document).ready(function(){
+// 		$("input[type='button']").click(function(){
+// 			var radioValue = $("input[name='red']:checked").val();
+// 			console.log(radioValue);
+// });
+// });
+
+
+
+function test() {
+	console.log(array);
+	
+	
+};
+
+
+
+
+
+
+
+
+
+
+
