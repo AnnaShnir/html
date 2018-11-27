54ed605866fdaf85e7ed49119c42781ceeb55d47
Nancy Lombardi <Nancy@Nancys-MacBook-Pro.local>
2018-11-27 15:22:51 -0500

    grabbing values of radio buttons and pushing them into an array, removing child after the question is answered

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-var array = 'vgh';
-
-// function answerZero() {
-// 	$(document).ready(function(){
-// 		$("input[type='button']").click(function(){
-// 			var radioValue = $("input[name='red']:checked").val();
-// 			console.log(radioValue);
-// });
-// });
+var owl = [];
 
 
 
-function test() {
-	console.log(array);
-	
+
+function answerQuestion1() {
+
+	var question0Value = document.querySelector("input[name='color']:checked").value;
+
+	var questionWrapper = document.getElementById("question-cards-wrapper");
+	questionWrapper.removeChild(questionWrapper.childNodes[1]);
+	questionWrapper;
+
+
+
+	// the problem here is inability of the user to go back and change the answer AND avoid repeating elements in the array. To solve this problem, push all var-s into the array at the end, after all questions have been answered and answer variables defined
+	owl.push(question0Value);
+	console.log(owl);
 	
 };
 
