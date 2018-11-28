cc89a8c1f25af4301dea01f28e2d4b0f6e5e1698
Nancy Lombardi <Nancy@Nancys-MacBook-Pro.local>
2018-11-28 12:36:45 -0500

    added Retake Test functionality

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-var owl = [];
+var answer = [];
 
 //here the var is declared outside the scope of the functions responsible for each answer. The functions are supposed to change values of these global variables, and pushed into an array from the button like "See my result"
 var question0Value;
 var question1Value;
 var question2Value;
 var question3Value;
+
+function pageRefresh() {
+	location.reload();
+}
 
 
 function answerQuestion0() {
@@ -17,8 +21,8 @@ function answerQuestion0() {
 	document.getElementById("question-1").style.display ="block";
 
 	// the problem here is inability of the user to go back and change the answer AND avoid repeating elements in the array. To solve this problem, push all var-s into the array at the end, after all questions have been answered and answer variables defined
-	owl.push(question0Value);
-	console.log(owl);
+	answer.push(question0Value);
+	console.log(answer);
 	
 };
 
@@ -33,8 +37,8 @@ function answerQuestion1() {
 
 
 	// the problem here is inability of the user to go back and change the answer AND avoid repeating elements in the array. To solve this problem, push all var-s into the array at the end, after all questions have been answered and answer variables defined
-	owl.push(question1Value);
-	console.log(owl);
+	answer.push(question1Value);
+	console.log(answer);
 	
 };
 
@@ -49,8 +53,8 @@ function answerQuestion2() {
 
 
 	// the problem here is inability of the user to go back and change the answer AND avoid repeating elements in the array. To solve this problem, push all var-s into the array at the end, after all questions have been answered and answer variables defined
-	owl.push(question2Value);
-	console.log(owl);
+	answer.push(question2Value);
+	console.log(answer);
 	
 };
 
@@ -66,10 +70,9 @@ function answerQuestion3() {
 	// document.getElementById("question-4").style.display ="block";
 
 
-
 	// the problem here is inability of the user to go back and change the answer AND avoid repeating elements in the array. To solve this problem, push all var-s into the array at the end, after all questions have been answered and answer variables defined
-	owl.push(question3Value);
-	console.log(owl);
+	answer.push(question3Value);
+	console.log(answer);
 	
 };
 
