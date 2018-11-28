6ecdddcd5cb20ad3691be34942a6fe5bf1ab27ca
Nancy Lombardi <Nancy@Nancys-MacBook-Pro.local>
2018-11-28 11:31:48 -0500

    finished hard-coding all the questions

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 var owl = [];
 
+//here the var is declared outside the scope of the functions responsible for each answer. The functions are supposed to change values of these global variables, and pushed into an array from the button like "See my result"
+var question0Value;
+var question1Value;
+var question2Value;
+var question3Value;
 
 
-
-function answerQuestion1() {
+function answerQuestion0() {
 
 	var question0Value = document.querySelector("input[name='color']:checked").value;
 
+// remove question0 card:
 	var questionWrapper = document.getElementById("question-cards-wrapper");
 	questionWrapper.removeChild(questionWrapper.childNodes[1]);
 	questionWrapper;
@@ -15,6 +20,57 @@ function answerQuestion1() {
 
 	// the problem here is inability of the user to go back and change the answer AND avoid repeating elements in the array. To solve this problem, push all var-s into the array at the end, after all questions have been answered and answer variables defined
 	owl.push(question0Value);
+	console.log(owl);
+	
+};
+
+function answerQuestion1() {
+
+	var question1Value = document.querySelector("input[name='pastime']:checked").value;
+
+// remove question0 card:
+	var questionWrapper = document.getElementById("question-cards-wrapper");
+	questionWrapper.removeChild(questionWrapper.childNodes[2]);
+	questionWrapper;
+
+
+
+	// the problem here is inability of the user to go back and change the answer AND avoid repeating elements in the array. To solve this problem, push all var-s into the array at the end, after all questions have been answered and answer variables defined
+	owl.push(question1Value);
+	console.log(owl);
+	
+};
+
+function answerQuestion2() {
+
+	var question2Value = document.querySelector("input[name='meet']:checked").value;
+
+// remove question0 card:
+	var questionWrapper = document.getElementById("question-cards-wrapper");
+	questionWrapper.removeChild(questionWrapper.childNodes[3]);
+	questionWrapper;
+
+
+
+	// the problem here is inability of the user to go back and change the answer AND avoid repeating elements in the array. To solve this problem, push all var-s into the array at the end, after all questions have been answered and answer variables defined
+	owl.push(question2Value);
+	console.log(owl);
+	
+};
+
+function answerQuestion3() {
+
+	var question3Value = document.querySelector("input[name='accomp']:checked").value;
+
+// remove question0 card:
+	var questionWrapper = document.getElementById("question-cards-wrapper");
+	questionWrapper.removeChild(questionWrapper.childNodes[4]);
+	questionWrapper;
+
+
+
+	// the problem here is inability of the user to go back and change the answer AND avoid repeating elements in the array. To solve this problem, push all var-s into the array at the end, after all questions have been answered and answer variables defined
+	owl.push(question3Value);
 	console.log(owl);
 	
 };
