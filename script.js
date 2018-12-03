9bc3a75f98a8bec409e7f352ecd7048fb6fa6b9c
Nancy Lombardi <Nancy@Nancys-MacBook-Pro.local>
2018-12-03 15:53:20 -0500

    hardcoding responses, WIP

--- script.js
+++ script.js
@@ -90,8 +90,44 @@ function testResult(){
 		// document.getElementById("winning-owl").addClass("style-for-winner");
 		console.log("New York Owl");
 	}
+	else if(result == "Rcaa" || "Rcab" || "Rcda" || "Rcdb" || "RNBeaa" || "RNBeaa" || "RNBeab" || "RNBeda" || "RNBedb" || "Raac" || "Raad" || "Radc" || "Radd" || "Rcac" || "Rcad" || "Rcdc" ||"Rcdd" || "RNBcbc" || "RNBcbd" || "RNBccc" || "RNBcdd"){
+		console.log("Geisha Owl");
+	}
+	else if(result == "Rdaa" || "Rdab" || "Rdda" || "Rddb" || "Rbba" || "Rbbb" || "Rdac" || "Rdad"  || "Rddc" || "Rddd"){
+		console.log("Moonlight Owl");
+	}
+	else if(result == "Reaa" || "Reab" || "Reda" || "Rebd" || "Reba" || "Rebb" || "Reca" || "Recb"  || "Oeaa" || "Oeab" || "Oeda" || "Oedb" || "Occa" || "Occb" || "Ocba" || "Ocbb" || "Reac" || "Read" || "Redc" || "Redd" || "Rcbc" || "Rcbd" || "Rccc" || "Rccd" || "Oaac" || "Oaad" || "Oadc" || "Oadd" || "Oeac" || "Oead" || "Oedc" || "Oedd" || "Ocbc" || "Ocbd" || "Occc" || "Occd"){
+		console.log("Phonix Owl");
+	}
+	else if(result == "Raba" || "Rabb" || "Raca" || "Racb" || "Yaba" || "Yabb" || "Yaca" || "Yacb"  || "Rebc" || "Rebd" || "Recc" || "Recd" || "Yeac" || "Yead" || "Yedc" || "Yedd" || "Yabc" || "Yabd" || "Yacc" || "Yacd"){
+		console.log("Spartan Owl");
+	}
+	else if(result == "Baaa" || "Baab" || "Bada" || "Badb" || "Bcba" || "Bcbb" || "Bcca" || "Bccb"  || "Beba" || "Bebb" || "Beca" || "Becb" || "Bcac" || "Bcad" || "Bcdc" || "Bcdd" || "Beac" || "Bead" || "Bedc" || "Bedd"){
+		console.log("Sailor Owl");
+	}
+	else if(result == "" || "" || "" || "" || "" || "" || "" || ""  || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || ""){
+		console.log("Upstate Owl");
+	}
+	else if(result == "" || "" || "" || "" || "" || "" || "" || ""  || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || ""){
+		console.log("Moonlight Owl");
+	}
+	else if(result == "" || "" || "" || "" || "" || "" || "" || ""  || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || ""){
+		console.log("Moonlight Owl");
+	}
+	else if(result == "" || "" || "" || "" || "" || "" || "" || ""  || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || "" || ""){
+		console.log("Moonlight Owl");
+	}
+
 	
-}
+	// use switch statement for break purposes:
+// 	switch(result()){
+// 		case 'Raaa': 
+// 		console.log("NY Owl switch statement worked");
+// 		break;
+// 		case 'Raab':
+// 		console.log("switch statement fired");
+// };
+};
 
 
 
